66b4f73214559a8ad949b459e71b0b7e0ecb9552
stmureiko <stanislav.mureiko_ges@omio.com>
2024-08-25 13:51:20 +0300

    new feature

--- gradle-module/src/main/java/org/example/Main.java
+++ gradle-module/src/main/java/org/example/Main.java
@@ -2,24 +2,7 @@ package org.example;
 
 public class Main {
 
-    public static final String CONSTANT = "constant";
-
     public static void main(String[] args) {
         System.out.println("Hello world!");
-    }
-
-    /*
-
-    asda
-
-    asd
-    as
-    d
-
-     */
-
-
-    private static void test() {
-        System.out.println("test");
     }
 }
