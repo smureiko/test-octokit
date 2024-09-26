b667e7b681487b5f0ac6c1a0add9df46d0db8516
stmureiko <stanislav.mureiko_ges@omio.com>
2024-09-26 15:17:36 +0200

    add calculator

---
+++ octokit-project/src/main/java/org/example/calculator/Calculator.java
@@ -0,0 +1,15 @@
+package org.example.calculator;
+
+public class Calculator {
+
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 2;
+
+        int c = a + b;
+
+        System.out.println("Sum of a and b is: " + c);
+
+
+    }
+}
