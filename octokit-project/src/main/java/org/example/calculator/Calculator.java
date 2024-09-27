cc375e9ce060ca9ac9da62a6137cf7d63278be3e
stmureiko <stanislav.mureiko_ges@omio.com>
2024-09-27 12:28:00 +0200

    fix comments

--- octokit-project/src/main/java/org/example/calculator/Calculator.java
+++ octokit-project/src/main/java/org/example/calculator/Calculator.java
@@ -3,13 +3,11 @@ package org.example.calculator;
 public class Calculator {
 
     public static void main(String[] args) {
-        int a = 1;
-        int b = 2;
-
-        int c = a + b;
-
-        System.out.println("Sum of a and b is: " + c);
-
-
+        System.out.println("Sum of firstNumber and secondNumber is: " + add(10, 20));
     }
+
+    private static int add(int firstNumber, int secondNumber) {
+        return firstNumber + secondNumber;
+    }
+
 }
